d930f5ac14a4377fd6dc62fe5ead370b6fe65d7e
patricia <prmonday@gmail.com>
2021-03-23 13:26:40 +0000

    README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # calculator-encapsulation
+```
+Exercise 1
+Use a diagram to group the methods in these examples:
+
+Bank
+  - get_balance
+  - credit_account(date, amount)
+  - debit_account(date, amount)
+  - print_statement
+SecretDiary
+  - lock
+  - unlock
+  - add_entry
+  - get_entries
+
+  ```
---
+++ lib/calculator.rb
@@ -0,0 +1,56 @@
+#Exercise 2
+
+class Calculator
+
+def initializer(number_1, number_2)
+  @number_1 = number_1
+  @number_2 = number_2
+  @answer = answer
+end
+
+
+def add(number_1, number_2)
+  @number_1 += @number_2
+end
+
+
+def subtract(number_1, number_2)
+  @number_1 -= @number_2
+end
+
+def print(answer)
+  "The Answer is : #{ answer }"
+end
+
+end
+
+
+# Exercise 3
+
+class Calculator
+
+def initializer(number_1, number_2)
+  @number_1 = number_1
+  @number_2 = number_2
+  @answer = answer
+end
+
+
+def add(number_1, number_2)
+  @number_1 += @number_2
+end
+
+
+def subtract(number_1, number_2)
+  @number_1 -= @number_2
+end
+
+end
+
+class Answer
+
+  def print(answer)
+  "The Answer is : #{ answer }"
+  end
+
+end
