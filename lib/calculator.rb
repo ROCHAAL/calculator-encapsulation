963548dd024c0027367d97c9feef94014f5466af
patricia <prmonday@gmail.com>
2021-03-24 13:34:49 +0000

    rewiriting diagram group for calculator

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,22 +2,18 @@
 
 class Calculator
 
-def initializer(number_1, number_2)
-  @number_1 = number_1
-  @number_2 = number_2
-  @answer = answer
-end
-
+group 1 (returning the addition value  of the inputs of an instance variable )
 
 def add(number_1, number_2)
-  @number_1 += @number_2
+  @number_1 + @number_2
 end
 
-
+group 2 (returning the subtraction value of the inputs of an instance variable)
 def subtract(number_1, number_2)
   @number_1 -= @number_2
 end
 
+group 3 ( printing)
 def print(answer)
   "The Answer is : #{ answer }"
 end
